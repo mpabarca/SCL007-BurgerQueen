97e023b360a238903a6a1d36e515b5ab3aa9a469
mpabarca <mpabarca@uc.cl>
2019-03-12 17:41:20 -0300

    se elimina codigo hasta ahora y archivos home.js y login.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import firebase from './fire';
-import Home from './home';
-import Login from './login';
+import fire from './firebase.js';
+import Home from './home.js';
+import Login from './login.js';
 
 import { breakfast } from "./breakfast.json";
+
+
+export default App;
+
 /*
 class App extends React.Component {
   constructor(props){
@@ -48,35 +52,3 @@ class App extends React.Component {
   }
 }
 */
-class App extends Component {
-  constructor() {
-    super();
-    this.state = ({
-      user: null,
-    });
-    this.authListener = this.authListener.bind(this);
-  }
-
-  componentDidMount() {
-    this.authListener();
-  }
-
-  authListener() {
-    fire.auth().onAuthStateChanged((user) => {
-      console.log(user);
-      if (user) {
-        this.setState({ user });
-        localStorage.setItem('user', user.uid);
-      } else {
-        this.setState({ user: null });
-        localStorage.removeItem('user');
-      }
-    });
-  }
-  render() {
-    return (
-     <div>{this.state.user ? : ( <Home/>) : (<Login />)}</div>
-    )}
-}
-
-export default App;
