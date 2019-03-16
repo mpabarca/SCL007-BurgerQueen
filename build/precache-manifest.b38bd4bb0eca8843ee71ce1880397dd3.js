3273ca0fff083d4852c3a73a7bf17178c12acf8d
mpabarca <mpabarca@uc.cl>
2019-03-16 11:41:56 -0300

    se hace dinner.json completo

---
+++ build/precache-manifest.b38bd4bb0eca8843ee71ce1880397dd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2cd599c0a34c11328c2d",
+    "url": "/static/js/main.37b14673.chunk.js"
+  },
+  {
+    "revision": "f733cd8443e37305289a",
+    "url": "/static/js/2.c6ac93ca.chunk.js"
+  },
+  {
+    "revision": "2cd599c0a34c11328c2d",
+    "url": "/static/css/main.a2ef154c.chunk.css"
+  },
+  {
+    "revision": "a1f2e520e8983cc4ae1b2921b0a3a4a4",
+    "url": "/index.html"
+  }
+];
