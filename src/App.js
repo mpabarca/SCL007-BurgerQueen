7ab27c333f1a14040f2b1698f34922d88bf245f4
mpabarca <mpabarca@uc.cl>
2019-03-27 22:04:38 -0300

    se ordenan componentes + se crea dinner.json acorde

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Order from './component-video/Order';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Order/>
+      
       </div>
     );
   }
